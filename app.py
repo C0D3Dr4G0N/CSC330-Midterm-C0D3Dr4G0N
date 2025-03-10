7eae95779d926452448dd5feee8fa0510c699e47
C0D3Dr4G0N <brennanj18@southernct.edu>
2025-03-10 18:45:28 -0400

    added subtraction function

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def add_numbers(a, b):
 def multiply_numbers(a, b):
         return str(a * b)
 
+@app.route('/subtr/<int:a>/<int:b>')
+def subtract_numbers(a, b):
+        return str(a - b)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
