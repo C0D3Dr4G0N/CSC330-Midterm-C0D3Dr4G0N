0973e2fde011232c506762785fbcfbdb722c64a7
C0D3Dr4G0N <brennanj18@southernct.edu>
2025-03-10 18:38:08 -0400

    added multiplication function

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/add/<int:a>/<int:b>')
+def add_numbers(a, b):
+        return str(a + b)
+
+@app.route('/mult/<int:a>/<int:b>')
+def multiply_numbers(a, b):
+        return str(a * b)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
